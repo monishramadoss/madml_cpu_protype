fixed flatten error, comming into gradient mismatching

madml/nn/pooling.py
@@ -59,36 +59,25 @@ class _MaxPoolNd(Module):
                     self.stride[i]) + 1
                 self._vol[i] = x.shape[i + 2]
             self.batch_size = x.shape[0] * x.shape[1]
-        self._2col(x.host_data)
+
+        if self.col is None:
+            n_output_plane = self.in_channels
+            output_length = self.batch_size
+            self.col = zeros([n_output_plane, output_length])
 
         max_idx = [0 for i in range(self.col.shape[1])]
-        # ARGMAX
-        for i in range(self.col.shape[0]):
-            for j in range(self.col.shape[1]):
-                m = self.col.host_data[i * self.col.shape[1] + j]
-                n = self.col.host_data[max_idx[j] * self.col.shape[1] + j]
-                if n < m:
-                    max_idx[j] = i
-
-        # y = B[max_idx, range(max_idx.size)]
         y = zeros([x.shape[0], x.shape[1], *self._col])
-        for i in range(self.col.shape[1]):
-            idx = max_idx[i]
-            y.host_data[i] = self.col.host_data[idx * self.col.shape[1] + i]
+
         self.cache.append(x)
-        self.cache.append(max_idx)
         self.cache.append(y)
+        self.cache.append(max_idx)
         return y
 
     def backward_cpu(self) -> None:
-        x, max_idx, y = self.cache
-        # dy_col = np.transpose(y.gradient, (2, 3, 4, 0, 1)).ravel()  # (72128,)
+        x, y, max_idx = self.cache
+        dx, dy = x.gradient, y.gradient
+        assert(x.shape == dx.shape and dy.shape == y.shape)
 
-        # self.col.gradient[max_idx, range(dy_col.size)] = dy_col
-        for i in range(self.col.shape[1]):
-            idx = max_idx[i]
-            self.col.gradient.host_data[idx * self.col.shape[1] + i] = y.gradient.host_data[i]
-        self._2vol(x.gradient.host_data)
         return x
 
     def _2col(self, x: List[Union[float, int, bytes, bool]]):
@@ -102,9 +91,6 @@ class _MaxPoolNd(Module):
             output_length *= c
             index_length *= c
             _col *= c
-
-        if self.col is None:
-            self.col = zeros([n_output_plane, output_length])
 
         for elt in range(self.batch_size):
             data_col = elt * self.in_channels * self._vol[0] * self._vol[1] * self._vol[2]

madml/tensor.py
@@ -23,26 +23,32 @@ def _convert_to_float(size: int, arr: List[bytes]) -> List[float]:
 class tensor(object):
     shape: List[int]
     init_shape: List[int]
-    host_data: List[Union[float, int, bytes, bool]]
+    host_memory: np.ndarray
+    device_memory: List[Union[float, int, bytes, bool]]
     on_device: bool
+    id: int
 
     def __init__(self, data: Union[List[Union[float, int, bytes, bool]], np.ndarray], shape: List[int] = []) -> None:
         if isinstance(data, np.ndarray):
-            self.host_data = data.reshape(-1).tolist()
-            self.shape = data.shape
+            self.host_memory = data
+            self.shape = list(data.shape)
+            self.device_memory = [data.ravel()[i] for i in range(data.size)]
         else:
-            self.host_data = data
+            self.host_memory = np.array(data).reshape(shape)
             self.shape = shape
+            self.device_memory = [data[i] for i in range(len(data))]
+
         self.init_shape = self.shape
-        assert (len(self.shape) > 0)
         self.size = 1
         for s in self.shape:
             self.size *= s
-        assert len(self.host_data) == self.size
-        _tensors.append(id(self))
+
         self.on_device = False
         self.parent = []
         self.children = []
+        self.id = id(self)
+        assert (len(self.shape) > 0)
+        assert (self.host_memory.size == self.size)
 
     def __len__(self):
         return self.shape[0]
@@ -53,59 +59,51 @@ class tensor(object):
         return self
 
     def numpy(self):
-        return np.array(self.host_data).reshape(self.shape)
+        return self.host_memory
 
     def __getitem__(self, idx: int):
-        assert (self.size > idx)
-        new_shape = self.shape[1:]
-        new_size = 1
-        for s in new_shape:
-            new_size *= s
-        new_data = [self.host_data[idx * new_size + i] for i in range(new_size)]
+        assert (self.shape[0] > idx)
+        new_data = self.host_memory[idx]
+        new_shape = self.shape[:idx]
         return tensor(new_data, new_shape)
 
     def __setitem__(self, key: int, value) -> None:
         assert (self.size > key)
         assert (type(value) == type(self))
-        for i in range(value.size):
-            self.host_data[key * value.size + i] = value.host_data[i]
+        self.host_memory[key] = value.host_memory
 
     def copy(self):
-        return tensor(self.host_data, self.shape)
+        return tensor(self.host_memory, self.shape)
 
     def reshape(self, shape: List[int]):
-        _size = 1
-        for s in shape:
-            _size *= s
-        if _size < 0:
-            s = self.size // _size
-            shape[shape.index(-1)] = abs(s)
-            _size *= s
-        _size = abs(_size)
-        if _size != self.size:
-            print(_size, self.size)
-            assert (_size == self.size)
-
-        assert(_size == len(self.host_data) and self.size == len(self.host_data))
-        self.shape = shape
+        self.host_memory.reshape(shape)
+        self.shape = list(self.host_memory.shape)
 
     @property
     def gradient(self):
-        if id(self) not in _gradients.keys():
-            _gradients[id(self)] = tensor([0 for _ in range(self.size)], self.shape)
-        return _gradients[id(self)]
+        if self.id not in _gradients.keys():
+            _gradients[self.id] = tensor([0 for _ in range(self.size)], self.init_shape)
+        assert (self.size == _gradients[self.id].size)
+        return _gradients[self.id]
 
     @property
     def grad_data(self):
-        if id(self) not in _gradients.keys():
-            _gradients[id(self)] = tensor([0 for _ in range(self.size)], self.shape)
-        return _gradients[id(self)]
+        grad = self.gradient.host_data
+        return grad.ravel()
+
+    @property
+    def host_data(self):
+        return self.host_memory
+
+    @property
+    def device_data(self):
+        return self.device_memory
 
     def backward(self):
         for x in reversed(self.parent):
-            if not x.visited[id(self)]:
+            if not x.visited[self.id]:
                 y = x.backward()
-                x.visited[id(self)] = True
+                x.visited[self.id] = True
                 if isinstance(y, tensor):
                     y.backward()
         self.parent.clear()
@@ -113,14 +111,18 @@ class tensor(object):
         return
 
     def numpy(self):
-        return np.array(self.host_data).reshape(self.shape)
+        return self.host_memory
 
     def reset(self):
         self.shape = self.init_shape
+        if self.id in _gradients.keys():
+            self.gradient.reshape(self.init_shape)
 
     def flatten(self):
         s = 1
         for S in self.shape[1:]:
             s *= S
         self.reshape([self.shape[0], s])
-        self.gradient.reshape([self.shape[0], s])
+        assert (self.shape[0] * s == self.size)
+        if self.id in _gradients.keys():
+            self.gradient.reshape(self.init_shape)

mnist_test.py
@@ -57,12 +57,11 @@ class mnist_model(nn.Module):
         super(mnist_model, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 46, 3)
-        self.fc1 = nn.Linear(46 * 12 * 12, 120)
+        self.conv2 = nn.Conv2d(32, 10, 3)
+        self.fc1 = nn.Linear(10 * 12 * 12, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
         self.relu1 = nn.ReLU()
-
         self.relu2 = nn.ReLU()
         self.relu3 = nn.ReLU()
         self.relu4 = nn.ReLU()
@@ -74,14 +73,16 @@ class mnist_model(nn.Module):
         x = self.conv2(x)  # 46 x 12 x 12
         x = self.relu2(x)
         x.flatten()
-        x = self.relu3(self.fc1(x))
-        x = self.relu4(self.fc2(x))
+        x = self.fc1(x)
+        x = self.relu3(x)
+        x = self.fc2(x)
+        x = self.relu4(x)
         x = self.fc3(x)
         return x
 
 
 def train_loop(model=mnist_model()):
-    batchsize = 32
+    batchsize = 16
     x, y, x1, y1 = load()
     x = x.reshape((-1, batchsize, 1, 1, 28, 28))
     x1 = x1.reshape((-1, 1, 1, 1, 28, 28))
@@ -96,6 +97,6 @@ def train_loop(model=mnist_model()):
         logit = model(t_x[i])
         loss = loss_fn(logit, t_y[i])
         loss.backward()
-        #optim.step()
+        optim.step()
         print('===', i, logit.shape, loss.host_data)
 train_loop()